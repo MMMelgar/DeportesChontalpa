64afb33f4a091995b57d7beeb45adf5ba944b37d
Monica <monicamelgar061@gmail.com>
2023-05-31 21:09:37 -0600

    1

--- app/src/main/java/com/example/Deportes_Chontalpa/Home.java
+++ app/src/main/java/com/example/Deportes_Chontalpa/Home.java
@@ -1,44 +1,63 @@
 package com.example.Deportes_Chontalpa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.Deportes_Chontalpa.Perfil.Perfil;
+
 
 public class Home extends AppCompatActivity {
 
-    private ImageButton btnMenu, btnCarrito, btnPerfil;
+    private ImageButton btnMenu, btnCarrito, btnPerfil, btnBusqueda;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Botones();
 
+    }
+
+    public void Botones(){
         btnMenu = findViewById(R.id.btnMenu);
         btnCarrito = findViewById(R.id.btnCarrito);
         btnPerfil = findViewById(R.id.btnPerfil);
+        btnBusqueda = findViewById(R.id.btnBusqueda);
 
         btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Lógica para el botón del menú
+                startActivity(new Intent(Home.this, Home.class));
+                finish();
             }
         });
 
         btnCarrito.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Lógica para el botón del carrito
+                startActivity(new Intent(Home.this, Carrito.class));
+                finish();
             }
         });
 
         btnPerfil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Lógica para el botón del perfil
+                startActivity(new Intent(Home.this, Perfil.class));
+                finish();
+            }
+        });
+
+        btnBusqueda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, Perfil.class));
+                finish();
             }
         });
     }
---
+++ app/src/main/java/com/example/Deportes_Chontalpa/Perfil/Perfil.java
@@ -0,0 +1,4 @@
+package com.example.Deportes_Chontalpa.Perfil;
+
+public class Perfil {
+}
